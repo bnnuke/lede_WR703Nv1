a6c5953f7e2a50037920414fa6225088df15117f
bnnuke <133558856+bnnuke@users.noreply.github.com>
2023-05-14 15:55:10 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -5,12 +5,13 @@ git clone "https://github.com/coolsnowwolf/lede.git"
 pushd "lede"; 
 
 git reset --hard e2535799fbfa1806694a5c5699f1bbe842868771
-sed -i -e 's/192.168.1.1/192.168.3.1/g' -e 's/OpenWrt/TPLink_WR703Nv1/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
+sed -i -e 's/192.168.1.1/192.168.1.11/g' -e 's/OpenWrt/TPLink_WR703Nv1/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
 sed -i '343s/4/16/g' target/linux/ar71xx/image/tiny-tp-link.mk  #修改wr703nv1的编译固件大小
 sed -i -e 's/OpenWrt/TPLink_WR703Nv1/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 修改WiFi名称,添加加密方式和密码
 
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default                                      #使用源码自带ShadowSocksR Plus软件
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default                                      #使用源码自带ShadowSocksR Plus软件
+echo 'src-git passwall https://github.com/1119176747/openwrt-package.git' >>feeds.conf.default
 sed -i -e '1s/$/^e82d730/g' feeds.conf.default
 sed -i -e '2s/$/^974fb04/g' feeds.conf.default
 sed -i -e '3s/$/^54fd237/g' feeds.conf.default
